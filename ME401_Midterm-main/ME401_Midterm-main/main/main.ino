c5166f49d5eb24d1d045ac8de253c80b2ecc47c1
InfinatEmber <34172919+InfinatEmber@users.noreply.github.com>
2021-10-19 13:28:14 -0700

    Add files via upload

---
+++ ME401_Midterm-main/ME401_Midterm-main/main/main.ino
@@ -0,0 +1,58 @@
+#include "ME401_Radio.h"
+#include "movement.h"
+
+enum StateMachineState {
+  HEALTHY = 0,
+  ZOMBIE = 1,
+  HEALING = 2,
+  INVALID = 3,
+  GET_BALL = 4
+};
+
+StateMachineState state = ZOMBIE;
+int team_id = 22;
+
+double x_conv = 0.001;//m per int
+double y_conv = 0.001;//m per int
+double a_conv = 0.001;//radians per int
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Setting up radio.");
+  setupRadio(team_id);
+  Serial.println("Setting up driving");
+  setup_driving();
+  Serial.println("Beggining loop");
+}
+
+void loop() {
+  Serial.println("Update Robot Ball Positions");
+  updateRobotPoseAndBallPositions();
+  Serial.println("Get State");
+  state = get_state();
+  
+  RobotPose current_pose = getRobotPose(team_id);
+  //printRobotPose(current_pose);
+  set_current_pos(x_conv*current_pose.x,y_conv*current_pose.y,a_conv*current_pose.theta);
+  change_speed_for_x_y(0.2,0.2);
+  update_speeds();
+  
+  delay(5000);
+}
+
+StateMachineState get_state() {
+  RobotPose current_pose = getRobotPose(team_id);
+  int n = current_pose.zombie + current_pose.healing*2;
+  if(n == 0) {
+    if(should_we_get_ball()){
+      n = 4;
+    }
+  }
+  Serial.print("State: ");
+  Serial.println(n);
+  return (StateMachineState) n;
+}
+
+int should_we_get_ball() {
+  return 0;
+}
